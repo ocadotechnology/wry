973164815de1a9f7623df3f8f042b6cca8f3b4ef
Ahmad Jagot <ahmad.jagot@ocado.com>
2015-11-09 18:24:59 +0000

    Implement boot medium selection.

--- TODO.md
+++ TODO.md
@@ -1,7 +1,6 @@
 - Proper logging
 - Make RESOURCE_URIs a json file?
-- Implement native RequestStateChange support...
-    - Perhaps source allowed methods for each resource from the schema
-- Make invoking instance method more DRY [in progress]
-- Add boot device selection, etc., support. [in progress]
 - Add SOL functionality
+- Make the operation timeout configurable
+- DONE: Reusable options (to stop selectors sticking around, amongst other things.)
+- DONE: Make invoking instance method more DRY
--- wry/tests/__init__.py
+++ wry/tests/__init__.py
@@ -15,27 +15,34 @@ import mock
 import pywsman
 import tempfile
 import os
-
 import wry
-
 from wry.tests import data
 
 
-class PowerTests(unittest.TestCase):
+class WryTest(unittest.TestCase):
     '''Tests for device power management/control.'''
 
     def setUp(self):
-        options = pywsman.ClientOptions()
-        client = pywsman.Client('fake_hostname', 16992, '/wsman', 'http', 'user', 'password')
+        super(WryTest, self).setUp()
+        self.options = pywsman.ClientOptions()
+        self.client = pywsman.Client('fake_hostname', 16992, '/wsman', 'http', 'user', 'password')
         fd, self.dumpfile_name = tempfile.mkstemp()
         self.dumpfile = pywsman.fopen(self.dumpfile_name, 'w')
-        client.set_dumpfile(self.dumpfile)
-        options.set_dump_request()
-        self.power = wry.device.AMTPower(client, options)
+        self.client.set_dumpfile(self.dumpfile)
+        self.options.set_dump_request()
 
     def tearDown(self):
         pywsman.fclose(self.dumpfile)
         os.remove(self.dumpfile_name)
+        super(WryTest, self).tearDown()
+
+
+class PowerTests(WryTest):
+    '''Tests for device power management/control.'''
+
+    def setUp(self):
+        super(PowerTests, self).setUp()
+        self.power = wry.device.AMTPower(self.client, self.options)
 
     @mock.patch('wry.decorators.CONNECT_RETRIES', 0)
     def test_power_on(self):
@@ -49,6 +56,96 @@ class PowerTests(unittest.TestCase):
                 data.power_state_change(2),
             )
 
+
+class KVMTests(WryTest):
+    '''Tests for device power management/control.'''
+
+    def setUp(self):
+        super(KVMTests, self).setUp()
+        self.kvm = wry.device.AMTKVM(self.client, self.options)
+
+    @mock.patch('wry.decorators.CONNECT_RETRIES', 0)
+    def test_kvm_enable(self):
+        try:
+            self.kvm.enabled = True
+        except wry.exceptions.AMTConnectFailure:
+            pass
+        with open(self.dumpfile_name, 'r') as output:
+            self.assertRegexpMatches(
+                output.read(),
+                data.kvm_enable(),
+            )
+
+
+class BootTests(WryTest):
+    '''Tests for device power management/control.'''
+
+    def setUp(self):
+        super(BootTests, self).setUp()
+        self.boot = wry.device.AMTBoot(self.client, self.options)
+
+    @mock.patch.multiple(pywsman.Client,
+        get=data.client_get,
+        enumerate=data.client_enumerate,
+        pull=data.client_pull_factory(),
+    )
+    @mock.patch('wry.decorators.CONNECT_RETRIES', 0)
+    def test_set_hdd_boot(self):
+        try:
+            self.boot.medium = 'Hard-Disk'
+        except wry.exceptions.AMTConnectFailure:
+            pass
+        with open(self.dumpfile_name, 'r') as output:
+            self.assertRegexpMatches(
+                output.read(),
+                data.set_boot_medium('Hard-Drive'),
+            )
+
+    @mock.patch.multiple(pywsman.Client,
+        get=data.client_get,
+        enumerate=data.client_enumerate,
+        pull=data.client_pull_factory(),
+    )
+    @mock.patch('wry.decorators.CONNECT_RETRIES', 0)
+    def test_set_cd_boot(self):
+        try:
+            self.boot.medium = 'CD/DVD'
+        except wry.exceptions.AMTConnectFailure:
+            pass
+        with open(self.dumpfile_name, 'r') as output:
+            self.assertRegexpMatches(
+                output.read(),
+                data.set_boot_medium('CD/DVD'),
+            )
+
+    @mock.patch.multiple(pywsman.Client,
+        get=data.client_get,
+        enumerate=data.client_enumerate,
+        pull=data.client_pull_factory(),
+    )
+    @mock.patch('wry.decorators.CONNECT_RETRIES', 0)
+    def test_set_pxe_boot(self):
+        try:
+            self.boot.medium = 'Network'
+        except wry.exceptions.AMTConnectFailure:
+            pass
+        with open(self.dumpfile_name, 'r') as output:
+            self.assertRegexpMatches(
+                output.read(),
+                data.set_boot_medium('Network'),
+            )
+
+    @mock.patch('wry.decorators.CONNECT_RETRIES', 0)
+    def test_set_boot_config_role(self):
+        try:
+            self.boot._set_boot_config_role()
+        except wry.exceptions.AMTConnectFailure:
+            pass
+        with open(self.dumpfile_name, 'r') as output:
+            self.assertRegexpMatches(
+                output.read(),
+                data.set_boot_config_role,
+            )
 
 if __name__ == '__main__':
     unittest.main()
